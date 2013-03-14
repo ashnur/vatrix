d8da2f27a9ac0041738af6449d379055e9f231bd
Ashnur <ashnur@gmail.com>
2013-03-14 17:26:53 +0200

    no need for node_modules in git

--- test/test.js
+++ test/test.js
@@ -28,14 +28,10 @@ describe("vatrix", function(){
     it("should be an object", function(){expect(vatrix).to.be.an('object')})
 
     describe("vectors", function(){
-        describe("scalar multiplication", function(){
+        describe("multiplication with a scalar value", function(){
             it("should multiply each element with the scalar", function(){
-                expect(vatrix.vm([1,3,5,7,11,13],13)).to
+                expect(vatrix.vsm([1,3,5,7,11,13],13)).to
                     .eql([13,39,65,91,143,169])
-            })
-            it("even if multiple scalars are provided", function(){
-                expect(vatrix.vm([1,3,2],2,3,5)).to
-                    .eql([30,90,60])
             })
         })
         describe("vector addition", function(){
@@ -48,6 +44,33 @@ describe("vatrix", function(){
                     .eql([13,17,5,7])
                 expect(vatrix.va([1,3,5,7],[,,12,14])).to
                     .eql([1,3,17,21])
+            })
+        })
+        describe("vector subtraction", function(){
+            it("should add values of the same indeces together", function(){
+                expect(vatrix.vs([1,3,5,7,11,13],[2,4,6,8,12,14])).to
+                    .eql([-1,-1,-1,-1,-1,-1])
+            })
+            it("missing or undefined values are considered zero", function(){
+                expect(vatrix.vs([1,3,5,7],[12,14])).to
+                    .eql([-11,-11,5,7])
+                expect(vatrix.vs([1,3,5,7],[,,12,14])).to
+                    .eql([1,3,-7,-7])
+            })
+        })
+        describe("dot product", function(){
+            it("should return the dot product of the input vectors", function(){
+                expect(vatrix.vdp([1,3,5],[7,11,13])).to
+                    .eql(105)
+            })
+            it("even with more than two vectors", function(){
+                expect(vatrix.vdp([ 1, 3, 7,11,13]
+                                 ,[ 7,11,13, 1, 3]
+                                 ,[ 3, 7, 1,13,11]
+                                 ,[11,13, 3, 7, 1]
+                                 ,[13, 1,11, 3, 7]
+                                 )).to
+                    .eql(3*5*7*11*13)
             })
         })
     })
