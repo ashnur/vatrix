61612e7d010305536ef04d4503cd4b5c6ec087fc
Ashnur <ashnur@gmail.com>
2013-03-26 19:35:28 +0200

    in middle of refactoring

--- index.js
+++ index.js
@@ -1,3 +1,266 @@
-// This file is just added for convenience so this repository can be
-// directly checked out into a project's deps folder
-module.exports = require('./lib/vatrix');
+void function(root){
+    'use strict'
+
+    var u = require('totemizer')
+        , m = require('momentum')
+        , r = m.r
+        , zero = r(0)
+        , one = r(1)
+        ;
+
+    function is_identity(matrix){
+        var result = true;
+        matrix.forEach(function(row, row_index){
+            if ( row_index < row.length && row[row_index] !== one )  {
+                result = false
+            }
+        })
+        return result
+    }
+
+    function is_upper_triangular(matrix){
+        var result = true;
+        matrix.forEach(function(row, row_index){
+            if ( row_index < row.length ) {
+                u.times(row_index, function(column_index){
+                    if ( row[column_index] !== zero ) {
+                        result = false
+                    }
+                })
+            }
+        })
+        return result
+    }
+
+    function is_lower_triangular(matrix){
+        return is_upper_triangular(vatrix.mt(matrix))
+    }
+
+    function is_diagonal(matrix){
+        return is_lower_triangular(matrix) && isUpperTriangular(matrix)
+    }
+
+
+    function draw_matrix(matrix){
+        return matrix.map(function(v,i){ return  v.join(' | ') }).join('\n')
+    }
+
+    // I am using underscores for internal functions
+    // and camelCase for the API
+
+    function interchange(arr, first, second){
+        arr = arr.slice(0)
+        var t = arr[first];
+        arr[first] = arr[second]
+        arr[second] = t
+        return arr
+    }
+
+    function pivot_row(width, index){
+        var row = m.zero_vector(width);
+        row[index] = 1
+        return row
+    }
+
+    function identity_matrix(n){
+        var I = [], i=0;
+        while ( i < n ) I.push(pivot_row(n,i++))
+        return I
+    }
+
+    function empty_matrix(width, height){
+        var Ø = [], i=0, j;
+        while ( i++ < height ) { j = 0; while ( j++ < width ) { Ø.push(0) } }
+        return Ø
+    }
+
+    function gcd(matrix){
+        return m.gcd(matrix.map(m.gcd))
+    }
+
+    function addition(){
+        return u.slice(arguments).reduce(function(p, c, i){
+            return u.zipWith(m.add, p, c)
+        })
+    }
+
+    function scalar_multiplication(matrix, scalar){
+        return matrix.map(function(vector){ return m.scale(vector, scalar) })
+    }
+
+    function scalar_division(matrix, scalar){
+        return matrix.map(function(vector){ return scalar_division(vector, scalar) })
+    }
+
+    function subtraction(){
+        return u.slice(arguments).reduce(function(p, c, i){
+            return u.zipWith(m.sub, p, c)
+        })
+    }
+
+
+    function transpose(matrix){
+        var height = matrix[0].length-1
+            , width = matrix.length-1
+            ;
+        return u.span(0, height).map(function(i){
+            return u.span(0, width).map(function(j){
+                return matrix[j][i]
+            })
+        })
+    }
+
+
+    function multiplication(){
+        return u.slice(arguments).reduce(function(A, B, index){
+            if ( ! Array.isArray(A[0]) ) A = [A]
+            if ( ! Array.isArray(B[0]) ) B = u.partition(B, 1)
+            var height = A.length-1
+                , width = (B = transpose(B)).length-1
+                , result = u.span(0, height).map(function(i){
+                    return u.span(0, width).map(function(j){
+                        return m.dot(A[i],B[j])
+                    })
+                })
+                ;
+            if ( result[0].length == 1) result = Array.prototype.concat.apply([],result)
+            if ( result.length == 1 ) result = result[0]
+
+            return result
+        })
+    }
+
+    function pivotize(matrix){
+        var size = matrix.length < matrix[0].length ? matrix.length : matrix[0].length
+            , P = vatrix(identity_matrix(matrix.length > matrix[0].length ? matrix.length : matrix[0].length))
+            , sign = 1
+            , row_index
+            , search
+            ;
+
+        for ( row_index = 0; row_index<size; row_index ++ ) {
+            if ( matrix[row_index][row_index] === zero ) {
+                for ( search = 0; search < size; search ++ ) {
+                    if ( search !== row_index
+                        && matrix[search][row_index] !== zero
+                        && matrix[row_index][search] !== zero
+                       ) {
+
+                        P = interchange(P, row_index, search)
+                        matrix = interchange(matrix, row_index, search)
+                        sign *= -1
+                        break
+                    }
+                }
+            }
+        }
+
+        return [P, r(sign)]
+    }
+
+    function echelon(matrix){
+
+        var width = matrix[0].length
+            , height = matrix.length
+            , pivotize_result = pivotize(matrix)
+            , augmentation = pivotize_result[0]
+            , change = pivotize_result[1]
+            , echelon = multiplication(augmentation, matrix)
+            ;
+
+
+        echelon.forEach(function(row, row_index){
+            u.times(row_index, function(column_index){
+                var pivot, pivot_row, lead, multiple;
+
+                pivot = echelon[column_index][column_index]
+                lead = echelon[row_index][column_index]
+//                multiple = r.lcm(pivot, lead)
+
+//                if ( multiple ) {
+//                    echelon[column_index] = m.scale(echelon[column_index], multiple.per(pivot))
+//                    augmentation[column_index] = m.scale(augmentation[column_index], multiple.per(pivot))
+                    echelon[row_index] = m.sub(echelon[row_index], m.scale(echelon[column_index],  lead.per(pivot)))
+                    augmentation[row_index] = m.sub(augmentation[row_index], m.scale(augmentation[column_index],  lead.per(pivot)))
+
+//
+//                    echelon[row_index] = m.scale(echelon[row_index], multiple.per(lead))
+//                    augmentation[row_index] = m.scale(augmentation[row_index], multiple.per(lead))
+//
+//                    change *= (multiple.per(pivot) * multiple.per(lead))
+//
+//                    pivot_row = echelon[column_index]
+//                    echelon[row_index] = m.sub(echelon[row_index], pivot_row)
+//                    augmentation[row_index] = m.sub(augmentation[row_index], pivot_row)
+//                }
+            })
+        })
+        return [echelon, change, augmentation]
+    }
+
+    function determinant(matrix){
+
+        if ( matrix.length != matrix[0].length) throw new Error ('not a square matrix')
+
+        var echelon = echelon(matrix);
+
+        return u.times(echelon[0].length, function(row_index){
+            return echelon[0][row_index][row_index]
+        }).reduce(multiply) / echelon[1]
+
+    }
+
+    function fully_reduce(matrix){
+        var echelon_result = echelon(matrix)
+            , echelon = echelon_result[0]
+            , change = echelon_result[1]
+            , augmentation = echelon_result[2]
+            ;
+
+        echelon.forEach(function(row, row_index){
+            var pivot = row[row_index];
+
+        })
+        return [echelon, augmentation]
+    }
+
+    function vatrix(arr){ return arr.map(m) }
+
+    vatrix.m = m
+
+    vatrix.matrixAddition = addition
+    vatrix.ma = addition
+
+    vatrix.matrixSubtraction = subtraction
+    vatrix.ms = subtraction
+
+    vatrix.matrixScalarMultiplication = scalar_multiplication
+    vatrix.msm = scalar_multiplication
+
+    vatrix.matrixTranspose = transpose
+    vatrix.mt = transpose
+
+    vatrix.matrixDeterminant = determinant
+    vatrix.md = determinant
+
+    vatrix.matrixMultiplication = multiplication
+    vatrix.mm = multiplication
+
+    vatrix.matrixRowEchelon = echelon
+    vatrix.me = echelon
+
+    vatrix.matrixRowReduce = fully_reduce
+    vatrix.mrr = fully_reduce
+
+    vatrix.drawMatrix = draw_matrix
+    vatrix.isLowerTriangular = is_lower_triangular
+    vatrix.isUpperTriangular = is_upper_triangular
+    vatrix.isDiagonal = is_diagonal
+
+    if ( module !== undefined && module.exports ) {
+        module.exports = vatrix
+    } else {
+        root.vatrix = vatrix
+    }
+
+}(this)
--- test/test.js
+++ test/test.js
@@ -1,231 +1,140 @@
-void function(root){
-    'use strict'
-    var vatrix = require('../')
-        , expect = require('expect.js')
-        ;
+require('better-stack-traces')
+'use strict'
+var vatrix = require('../')
+    , expect = require('expect.js')
+    , u = require('totemizer')
+    , m = vatrix.m
+    , r = vatrix.m.r
+    ;
 
-    function times(nr, fun) {
-        var result = []
-            , i
-            ;
-        for ( i = 0; i < nr; i++) { result.push(fun(i)) }
-        return result
-    }
+describe('vectors', function(){
 
-    function rand(min, max){
-        var r =  Math.floor(Math.random() * (max - min + 1)) + min;
-        return  r == 0 ?  rand(max, min) : r
-    }
-
-    function getRandomArray(minLength, maxLength){
-
-        var length = rand(maxLength || 6, minLength || 3)
-            , common_factor = rand(13, -13)
-            , arr=[]
-            ;
-
-        while ( maxLength-- > minLength ) {
-            arr.push(rand(13, -13)*common_factor)
-        }
-
-        arr.push(rand(13, -13)*common_factor)
-
-        return arr
-    }
-
-    function isIdentity(matrix){
-        var result = true;
-        matrix.forEach(function(row, row_index){
-            if ( row_index < row.length && row[row_index] !== 1 ) {
-                result = false
-            }
+    describe('matrix addition', function(){
+        it('should add values of the same indeces together', function(){
+            expect(vatrix.ma(vatrix([[1,2],[3,4]]), vatrix([[9,8],[7,6]]))).to
+                .eql(vatrix([[10,10],[10,10]]))
         })
-        return result
-    }
+        it('should add values of the same indeces together', function(){
+            expect(vatrix.ma(vatrix([[1,2],[3,4],[0,0]])
+                            ,vatrix([[11,12],[21,22],[31,32]])
+                            )).to
+                .eql(vatrix([[12,14],[24,26],[31,32]]))
 
-    function isLowerTriangular(matrix){
-        var result = true;
-        matrix.forEach(function(row, row_index){
-            if ( row_index < row.length ) {
-                times(row_index, function(column_index){
-                    if ( row[column_index] !== 0 ) {
-                        result = false
-                    }
-                })
-            }
-        })
-        return result
-    }
-
-    function isUpperTriangular(matrix){
-        return isLowerTriangular(vatrix.mt(matrix))
-    }
-
-    function isDiagonal(matrix){
-        return isLowerTriangular(matrix) && isUpperTriangular(matrix)
-    }
-
-    function drawMatrix(matrix){
-        return matrix.map(function(v,i){ return  v.join(' | ') }).join('\n')
-    }
-    it('should be an object', function(){expect(vatrix).to.be.an('object')})
-
-    describe('vectors', function(){
-        describe('multiplication with a scalar value', function(){
-            it('should multiply each element with the scalar', function(){
-                expect(vatrix.vsm([1,3,5,7,11,13],13)).to
-                    .eql([13,39,65,91,143,169])
-            })
-        })
-        describe('vector addition', function(){
-            it('should add values of the same indeces together', function(){
-                expect(vatrix.va([1,3,5,7,11,13],[2,4,6,8,12,14])).to
-                    .eql([3,7,11,15,23,27])
-            })
-        })
-        describe('vector subtraction', function(){
-            it('should add values of the same indeces together', function(){
-                expect(vatrix.vs([1,3,5,7,11,13],[2,4,6,8,12,14])).to
-                    .eql([-1,-1,-1,-1,-1,-1])
-            })
-        })
-        describe('dot product', function(){
-            it('should return the dot product of the input vectors', function(){
-                expect(vatrix.vdp([1,3,5],[7,11,13])).to
-                    .eql(105)
-            })
-            it('even with more than two vectors', function(){
-                expect(vatrix.vdp([ 1, 3, 7,11,13]
-                                 ,[ 7,11,13, 1, 3]
-                                 ,[ 3, 7, 1,13,11]
-                                 ,[11,13, 3, 7, 1]
-                                 ,[13, 1,11, 3, 7]
-                                 )).to
-                    .eql(3*5*7*11*13)
-            })
-        })
-
-        describe('matrix addition', function(){
-            it('should add values of the same indeces together', function(){
-                expect(vatrix.ma([[1,2],[3,4]],[[9,8],[7,6]])).to
-                    .eql([[10,10],[10,10]])
-            })
-            it('should add values of the same indeces together', function(){
-                expect(vatrix.ma([[1,2],[3,4]]
-                                ,[[11,12],[21,22],[31,32]]
-                                ,[[9,8],[7,6]]
-                                ,[[19,29,39],[49,59,69]]
-                                ,[[60,49,61],[20,9,31],[69,68,100]]
-                                )).to
-                    .eql([[100,100,100],[100,100,100],[100,100,100]])
-            })
-        })
-
-        describe('matrix subtraction', function(){
-            it('should add values of the same indeces together', function(){
-                expect(vatrix.ms([[1,2],[3,4]],[[9,8],[7,6]])).to
-                    .eql([[-8,-6],[-4,-2]])
-            })
-            it('should add values of the same indeces together', function(){
-                expect(vatrix.ms([[1,2],[3,4]]
-                                ,[[11,12],[21,22],[31,32]]
-                                ,[[9,8],[7,6]]
-                                ,[[19,29,39],[49,59,69]]
-                                ,[[60,49,61],[20,9,31],[69,68,100]]
-                                )).to
-                    .eql([[-98,-96,-100],[-94,-92,-100],[-100,-100,-100]])
-            })
-        })
-
-        describe('matrix multiplication with scalar value', function(){
-            it('should multiply all the coefficients', function(){
-                expect(vatrix.msm([[1,2],[3,4]],3)).to.eql([[3,6],[9,12]])
-            })
-        })
-        describe('matrix transpose', function(){
-            it('should return the dot product of the input vectors', function(){
-                expect(vatrix.mt([[1,3,5],[7,11,13]])).to
-                    .eql([[1,7],[3,11],[5,13]])
-            })
-        })
-
-        describe('matrix multiplication', function(){
-            it('should return a vector ', function(){
-                expect(vatrix.mm([[15,25],[42,69]], [2,4])).to.eql([130,360])
-            })
-            it('should return a vector ', function(){
-                expect(vatrix.mm([2,4], [[15,25],[42,69]])).to.eql([198,326])
-            })
-            it('should return the result matrix', function(){
-                expect(vatrix.mm([[1,3],[5,7]], [[2,4],[6,8]])).to
-                    .eql([[20,28],[52,76]])
-            })
-            it('should return the result matrix', function(){
-                expect(vatrix.mm([[2,3,4],[5,6,7]], [[1,8],[9,2],[3,4]])).to
-                    .eql([[41,38],[80,80]])
-            })
-            it('should return the result matrix', function(){
-                expect(vatrix.mm([[1,3],[5,7],[11,13]],[[17,19,23],[29,31,37]])).to
-                    .eql([[104,112,134],[288,312,374],[564,612,734]])
-            })
-            it('should return the result matrix', function(){
-                expect(vatrix.mm([[1,3],[5,7],[11,13]],[[17,19,23],[29,31,37]],[[1,2],[3,4],[4,6]])).to
-                    .eql([[4*244,4*365],[4*680,4*1017],[4*1334,4*1995]])
-            })
-        })
-
-        describe('matrix echelon form', function(){
-            function test(matrix){
-                var e = vatrix.me(matrix);
-                //console.log('ze matrix ------------------')
-                //console.log(drawMatrix(matrix))
-                //console.log('e echelon ------------------')
-                //console.log(drawMatrix(e[0]))
-                //console.log('augmentation ------------------')
-                //console.log(drawMatrix(e[2]))
-                expect(isLowerTriangular(e[0])).to.equal(true)
-            }
-            it('should return an upper a lower triangular matrix, and a sign change', function(){
-                test([[3,7],[14,22]])
-                test([[32, 551, 23],[391, 122, 123],[332, 13, 832]])
-                test([[0,2],[1,0],[3,4]])
-                test([[1,2],[1,-1],[3,4]])
-                test([[32, 551, 23, 111],[391, 12, 122, 123],[2, 332, 13, 832]])
-                test([[32, 551, 23], [111, 391, 12], [122, 123, 2], [332, 13, 832]])
-            })
-        })
-
-        describe('matrix determinant', function(){
-            it('should return the determinant', function(){
-                expect(vatrix.md([[3,0,5],[0,0,4],[0,9,3]])).to.eql(-108)
-                expect(vatrix.md([[15,75],[60,24]])).to.eql(-4140)
-                expect(vatrix.md([[15,75,2],[5,30,82],[45,90,6]])).to.eql(164700)
-                expect(vatrix.md([[5,30,82],[15,75,2],[45,90,6]])).to.eql(-164700)
-                expect(vatrix.md([[1,3,5],[2,4,7],[1,1,0]])).to.eql(4)
-            })
-        })
-
-        describe('matrix row reduce', function(){
-
-            function test(matrix){
-                var e = vatrix.mrr(matrix);
-                //console.log('ze matrix ------------------')
-                //console.log(drawMatrix(matrix))
-                //console.log('e echelon ------------------')
-                //console.log(drawMatrix(e[0]))
-                //console.log('augmentation ------------------')
-                //console.log(drawMatrix(e[1]))
-                expect(isDiagonal(e[0], true)).to.equal(true)
-            }
-            it('should return an the identity and the inverse matrices of the input', function(){
-                test([[3,7],[14,22]])
-                test([[32, 551, 23],[391, 122, 123],[332, 13, 832]])
-                test([[0,2],[1,0],[3,4]])
-                test([[1,2],[1,-1],[3,4]])
-                test([[32, 551, 23, 111],[391, 12, 122, 123],[2, 332, 13, 832]])
-                test([[32, 551, 23], [111, 391, 12], [122, 123, 2], [332, 13, 832]])
-            })
-
+            expect(vatrix.ma(vatrix([[1,2,0],[3,4,0],[0,0,0]])
+                            ,vatrix([[11,12,0],[21,22,0],[31,32,0]])
+                            ,vatrix([[9,8,0],[7,6,0],[0,0,0]])
+                            ,vatrix([[19,29,39],[49,59,69],[0,0,0]])
+                            ,vatrix([[60,49,61],[20,9,31],[69,68,100]])
+                            )).to
+                .eql(vatrix([[100,100,100],[100,100,100],[100,100,100]]))
         })
     })
-}(this)
+
+    describe('matrix subtraction', function(){
+        it('should add values of the same indeces together', function(){
+            expect(vatrix.ms(vatrix([[1,2],[3,4]]),vatrix([[9,8],[7,6]]))).to
+                .eql(vatrix([[-8,-6],[-4,-2]]))
+        })
+        it('should add values of the same indeces together', function(){
+            expect(vatrix.ms(vatrix([[1,2,0],[3,4,0],[0,0,0]])
+                            ,vatrix([[11,12,0],[21,22,0],[31,32,0]])
+                            ,vatrix([[9,8,0],[7,6,0],[0,0,0]])
+                            ,vatrix([[19,29,39],[49,59,69],[0,0,0]])
+                            ,vatrix([[60,49,61],[20,9,31],[69,68,100]])
+                            )).to
+                .eql(vatrix([[-98,-96,-100],[-94,-92,-100],[-100,-100,-100]]))
+        })
+    })
+
+    describe('matrix multiplication with scalar value', function(){
+        it('should multiply all the coefficients', function(){
+            expect(vatrix.msm(vatrix([[1,2],[3,4]]),r(3))).to.eql(vatrix([[3,6],[9,12]]))
+        })
+    })
+    describe('matrix transpose', function(){
+        it('should return the dot product of the input vectors', function(){
+            expect(vatrix.mt(vatrix([[1,3,5],[7,11,13]]))).to
+                .eql(vatrix([[1,7],[3,11],[5,13]]))
+        })
+    })
+
+    describe('matrix multiplication', function(){
+        it('should return a vector ', function(){
+            expect(vatrix.mm(vatrix([[15,25],[42,69]]), m([2,4]))).to.eql(m([130,360]))
+        })
+        it('should return a vector ', function(){
+            expect(vatrix.mm(m([2,4]), vatrix([[15,25],[42,69]]))).to.eql(m([198,326]))
+        })
+        it('should return the result matrix', function(){
+            expect(vatrix.mm(vatrix([[1,3],[5,7]]), vatrix([[2,4],[6,8]]))).to
+                .eql(vatrix([[20,28],[52,76]]))
+        })
+        it('should return the result matrix', function(){
+            expect(vatrix.mm(vatrix([[2,3,4],[5,6,7]]), vatrix([[1,8],[9,2],[3,4]]))).to
+                .eql(vatrix([[41,38],[80,80]]))
+        })
+        it('should return the result matrix', function(){
+            expect(vatrix.mm(vatrix([[1,3],[5,7],[11,13]]),vatrix([[17,19,23],[29,31,37]]))).to
+                .eql(vatrix([[104,112,134],[288,312,374],[564,612,734]]))
+        })
+        it('should return the result matrix', function(){
+            expect(vatrix.mm(vatrix([[1,3],[5,7],[11,13]]),vatrix([[17,19,23],[29,31,37]])
+                        ,vatrix([[1,2],[3,4],[4,6]]))).to
+                .eql(vatrix([[4*244,4*365],[4*680,4*1017],[4*1334,4*1995]]))
+        })
+    })
+
+    describe('matrix echelon form', function(){
+        function test(matrix){
+            console.log('ze matrix ------------------')
+            console.log(vatrix.drawMatrix(matrix))
+            var e = vatrix.me(vatrix(matrix));
+            console.log('e echelon ------------------')
+            console.log(vatrix.drawMatrix(e[0]))
+            console.log('augmentation ------------------')
+            console.log(vatrix.drawMatrix(e[2]))
+            expect(vatrix.isUpperTriangular(e[0])).to.equal(true)
+        }
+        it('should return an upper triangular matrix, and a sign change', function(){
+            test([[3,7],[14,22]])
+            test([[32, 551, 23],[391, 122, 123],[332, 13, 832]])
+            test([[0,2],[1,0],[3,4]])
+            test([[1,2],[1,-1],[3,4]])
+            test([[32, 551, 23, 111],[391, 12, 122, 123],[2, 332, 13, 832]])
+            test([[32, 551, 23], [111, 391, 12], [122, 123, 2], [332, 13, 832]])
+        })
+    })
+
+    describe('matrix determinant', function(){
+        it('should return the determinant', function(){
+            expect(vatrix.md(vatrix([[3,0,5],[0,0,4],[0,9,3]]))).to.eql(r(-108))
+            expect(vatrix.md(vatrix([[15,75],[60,24]]))).to.eql(r(-4140))
+            expect(vatrix.md(vatrix([[15,75,2],[5,30,82],[45,90,6]]))).to.eql(r(164700))
+            expect(vatrix.md(vatrix([[5,30,82],[15,75,2],[45,90,6]]))).to.eql(r(-164700))
+            expect(vatrix.md(vatrix([[1,3,5],[2,4,7],[1,1,0]]))).to.eql(r(4))
+        })
+    })
+
+    describe('matrix row reduce', function(){
+
+        function test(matrix){
+            var e = vatrix.mrr(vatrix(matrix));
+            //console.log('ze matrix ------------------')
+            //console.log(drawMatrix(matrix))
+            //console.log('e echelon ------------------')
+            //console.log(drawMatrix(e[0]))
+            //console.log('augmentation ------------------')
+            //console.log(drawMatrix(e[1]))
+            expect(vatrix.isDiagonal(e[0], true)).to.equal(true)
+        }
+        it('should return an the identity and the inverse matrices of the input', function(){
+            test([[3,7],[14,22]])
+            test([[32, 551, 23],[391, 122, 123],[332, 13, 832]])
+            test([[0,2],[1,0],[3,4]])
+            test([[1,2],[1,-1],[3,4]])
+            test([[32, 551, 23, 111],[391, 12, 122, 123],[2, 332, 13, 832]])
+            test([[32, 551, 23], [111, 391, 12], [122, 123, 2], [332, 13, 832]])
+        })
+
+    })
+})
